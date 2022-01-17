4f3eef7340a5cc55beb331bfaadc79d133d38779
akshata2005 <akshatasawant1805@gmail.com>
2022-01-17 19:55:51 +0530

    [akshata] Adding UC1-SinglePlayer

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //single position at start
+            Console.WriteLine("Welcome to snake and ladder program");  //Welcome message
+            int NO_OF_PLAYER = 1;              //number of player
+            int POSITION = 0;                 //position of player
+            Console.WriteLine("Number of player: " + NO_OF_PLAYER + " is playing in game at position: " + POSITION);
+
+        }
+    }
+}
